3a1b764a5612407325ec7c3496ff302e62ffee97
zydabdulaz <anneardysa@gmail.com>
2025-10-01 20:45:11 +0700

    Add files via upload

---
+++ SuccessPopup.cs
@@ -0,0 +1,34 @@
+﻿namespace ArdysaModsTools
+{
+    public partial class SuccessPopup : Form
+    {
+        public SuccessPopup()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            this.Close(); // Simply closes the pop-up
+        }
+
+        private void launchDotaButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Launch Dota 2 via Steam
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = "steam://rungameid/570",
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to launch Dota 2: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            this.Close(); // Close the pop-up after launching
+        }
+    }
+}
