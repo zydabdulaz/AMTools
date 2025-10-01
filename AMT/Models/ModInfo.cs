23c18de4dc51618234d489df950389b7d07f2cc3
zydabdulaz <anneardysa@gmail.com>
2025-10-01 22:12:44 +0700

    Create ModInfo.cs

---
+++ AMT/Models/ModInfo.cs
@@ -0,0 +1,9 @@
+namespace AMTools.Models
+{
+    public class ModInfo
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Path { get; set; }
+    }
+}
