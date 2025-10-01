1dd587e4fa830f7f096114546c8968ce8214701b
zydabdulaz <anneardysa@gmail.com>
2025-10-01 22:47:58 +0700

    Update LoggerService.cs

--- AMT/Sevices/LoggerService.cs
+++ AMT/Sevices/LoggerService.cs
@@ -1,11 +1,40 @@
 using System;
+using System.Drawing;
+using System.Windows.Forms;
 
-namespace AMTools.Services
+namespace ArdysaModsTools.Services
 {
     public class LoggerService
     {
-        public void Info(string message) => Console.WriteLine($"[INFO] {message}");
-        public void Warn(string message) => Console.WriteLine($"[WARN] {message}");
-        public void Error(string message) => Console.WriteLine($"[ERROR] {message}");
+        private readonly RichTextBox _console;
+
+        public LoggerService(RichTextBox console)
+        {
+            _console = console;
+        }
+
+        public void Log(string msg) => Write(msg, _console.ForeColor);
+        public void Info(string msg) => Write(msg, Color.LightGray);
+        public void Warn(string msg) => Write(msg, Color.Orange);
+        public void Error(string msg) => Write(msg, Color.Red);
+
+        private void Write(string msg, Color color)
+        {
+            if (_console.InvokeRequired)
+            {
+                _console.Invoke(new Action(() => Write(msg, color)));
+                return;
+            }
+
+            string timestamp = DateTime.Now.ToString("HH:mm");
+            string text = $"[{timestamp}] {msg}\r\n";
+
+            int start = _console.TextLength;
+            _console.AppendText(text);
+            _console.SelectionStart = start;
+            _console.SelectionLength = text.Length;
+            _console.SelectionColor = color;
+            _console.ScrollToCaret();
+        }
     }
 }
